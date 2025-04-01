304de7d0c586db345c17f80632173a0a9d879630
Roy Vanegas <roy@thecodeeducators.com>
2025-04-01 13:38:17 -0400

    Implement an HTML compressor

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
 const { src, dest, series, watch } = require(`gulp`),
-    htmlValidator = require(`gulp-html`);
+    htmlValidator = require(`gulp-html`),
+    htmlCompressor = require(`gulp-htmlmin`);
 
 let validateHTML = () => {
     return src(`index.html`)
         .pipe(htmlValidator());
 };
 
+let compressHTML = () => {
+    return src(`index.html`)
+        .pipe(htmlCompressor({collapseWhitespace:true}))
+        .pipe(dest(`prod`));
+};
+
 exports.validateHTML = validateHTML;
+exports.compressHTML = compressHTML;
